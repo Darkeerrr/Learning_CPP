553f26b86ab1dde53f73ce7ac7af90b7a751d518
Darker <darker2045@gmail.com>
2025-05-03 08:36:09 +0300

    Добавил решение с задачей про кузнечика

---
+++ problems/group_1/Grasshopper/Grasshopper.cpp
@@ -0,0 +1,17 @@
+﻿#include "game.h"
+
+using namespace std::literals;
+
+int main() {
+    std::setlocale(LC_ALL, "Russian");
+    int board_size, scores;
+    std::cout << "Размер поля: "s << std::endl;
+    std::cin >> board_size;
+    std::cout << "Сколько очков нужно для победы: "s << std::endl;
+    std::cin >> scores;
+
+    std::random_device rd;
+
+    Game game{ rd(), board_size, scores };
+    game.Start();
+}
