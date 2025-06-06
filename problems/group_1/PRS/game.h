2ef879d6531ae9212670961fbb1c96aa0ec3dbfb
Darker <darker2045@gmail.com>
2025-06-07 02:14:15 +0300

    Добавлены файлы через LFS

---
+++ problems/group_1/PRS/game.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:434ff1218b9647b3b4838069285dbd06acfee4f7e1c1f3808417cca8246fb3ab
+size 3023
---
+++ problems/group_1/PRS/main.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0599e501de6187cac65d600c989488e010f2a8c0e1e498f09c00925dc7f1a3b3
+size 232
